db25aa36d318117755f495f4b17eed7bcd100ff1
Alojży <alojzy231@gmail.com>
2021-01-30 21:05:53 +0100

    Added swiping animation

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,11 +1,15 @@
 window.onload = function(){
 
-    let answer_1 = document.getElementById("answer1-content");
-    let answer_2 = document.getElementById("answer2-content");
-    let question = document.getElementById("question-content");
+    let answer1 = document.getElementById("answer1-card");
+    let answer2 = document.getElementById("answer2-card");
+    let animatedQuestionCard = document.getElementById("animated-question-card");
+
+    let answer1Content = document.getElementById("answer1-content");
+    let answer2Content = document.getElementById("answer2-content");
+    let questionContent = document.getElementById("question-content");
+    let animatedQuestionContent = document.getElementById("animated-question-content");
 
     newTour();
-
 
     function newTour(){
         fetch('http://localhost:5000/get')
@@ -14,10 +18,29 @@ window.onload = function(){
     }
 
     function update(data){
-        answer_1.innerText = data.answer_1;
-        answer_2.innerText = data.answer_2;
-        question.innerText = data.question;
+        answer1Content.innerText = data.answer_1;
+        answer2Content.innerText = data.answer_2;
+        questionContent.innerText = data.question;
     }
+
+    let duration = 1;
+
+    answer1.onclick=function(){
+        animatedQuestionContent.innerText = questionContent.innerText;
+        animatedQuestionCard.style.animation = "";
+        animatedQuestionCard.style.animation = `left ${duration}s 1`;
+        newTour();
+        setTimeout(function(){animatedQuestionCard.style.animation = "";},duration * 1000);
+    }
+
+    answer2.onclick=function(){
+        animatedQuestionContent.innerText = questionContent.innerText;
+        animatedQuestionCard.style.animation = "";
+        animatedQuestionCard.style.animation = `right ${duration}s 1`;
+        newTour();
+        setTimeout(function(){animatedQuestionCard.style.animation = "";},duration * 1000);
+    }
+
 }
 
 
